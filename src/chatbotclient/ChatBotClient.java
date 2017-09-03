693447f381be3770ce6cf6edaaddc0d394c6be1e
DusanT <DusanT@Dusan>
2017-09-03 14:16:04 +0200

    Client strana

---
+++ src/chatbotclient/ChatBotClient.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatbotclient;
+
+import java.io.BufferedReader;
+import java.net.Socket;
+
+/**
+ *
+ * @author DusanT
+ */
+public class ChatBotClient {
+
+    static Socket comunicationSocket;
+    static BufferedReader console;
+    static BufferedReader inputStream;
+   
+    public static void main(String[] args) {
+        
+    }
+    
+}
