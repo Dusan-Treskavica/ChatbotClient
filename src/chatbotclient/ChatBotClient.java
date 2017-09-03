26a459f8aa599b12545f09141aa10074a1c8944b
DusanT <DusanT@Dusan>
2017-09-03 15:05:23 +0200

    Client strana

--- src/chatbotclient/ChatBotClient.java
+++ src/chatbotclient/ChatBotClient.java
@@ -6,20 +6,61 @@
 package chatbotclient;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author DusanT
  */
-public class ChatBotClient {
+public class ChatBotClient implements Runnable{
 
-    static Socket comunicationSocket;
+    static Socket communicationSocket;
     static BufferedReader console;
     static BufferedReader inputStream;
-   
+    static PrintStream outputStream;
+    
     public static void main(String[] args) {
+        try {
+            communicationSocket = new Socket("localhost", 5555);
+            console = new BufferedReader(
+                        new InputStreamReader(
+                                System.in));
+            
+            inputStream = new BufferedReader(
+                            new InputStreamReader(
+                                    communicationSocket.getInputStream()));
+            outputStream = new PrintStream(
+                                communicationSocket.getOutputStream());
+            
+            new Thread(new ChatBotClient()).start();
+            
+            outputStream.println("hello");
+            while(true){
+                outputStream.println(console.readLine());
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ChatBotClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void run() {
+        //String poruka;
+        try {
+            while(true){
         
+                System.out.println(inputStream.readLine());
+            
+            }
+        }catch (IOException ex) {
+                Logger.getLogger(ChatBotClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
     
 }
